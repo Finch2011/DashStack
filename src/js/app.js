cbf32860fef82b195fbb351f76fc704053e3c8f5
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2024-03-02 11:22:12 -0800

    end landing page

---
+++ src/js/app.js
@@ -0,0 +1,129 @@
+const st = document.getElementById("st")
+const st2 = document.getElementById("st2")
+const st3 = document.getElementById("st3")
+const st4 = document.getElementById("st4")
+const st5 = document.getElementById("st5")
+const st6 = document.getElementById("st6")
+const st7 = document.getElementById("st7")
+const st8 = document.getElementById("st8")
+const st9 = document.getElementById("st9")
+const st10 = document.getElementById("st10")
+const st11 = document.getElementById("st11")
+const st12 = document.getElementById("st12")
+const st13 = document.getElementById("st13")
+const st14 = document.getElementById("st14")
+const st15 = document.getElementById("st15")
+const st16 = document.getElementById("st16")
+const stImg = document.getElementById("st-img")
+const stImg2 = document.getElementById("st-img2")
+const stImg3 = document.getElementById("st-img3")
+const stImg4 = document.getElementById("st-img4")
+const stImg5 = document.getElementById("st-img5")
+const stImg6 = document.getElementById("st-img6")
+const stImg7 = document.getElementById("st-img7")
+const stImg8 = document.getElementById("st-img8")
+const stImg9 = document.getElementById("st-img9")
+const stImg10 = document.getElementById("st-img10")
+const stImg11 = document.getElementById("st-img11")
+const stImg12 = document.getElementById("st-img12")
+const stImg13 = document.getElementById("st-img13")
+const stImg14 = document.getElementById("st-img14")
+const stImg15 = document.getElementById("st-img15")
+const stImg16 = document.getElementById("st-img16")
+st.addEventListener("mouseenter" , ()=>{
+    stImg.src = "../asests/ 1.svg"
+})
+st.addEventListener("mouseleave" , ()=>{
+    stImg.src = "../asests/.svg"
+})
+st2.addEventListener("mouseenter" , ()=>{
+    stImg2.src = "../asests/my-2.svg"
+})
+st2.addEventListener("mouseleave" , ()=>{
+    stImg2.src = "../asests/m-1.svg"
+})
+st3.addEventListener("mouseenter", () =>{
+    stImg3.src = "../asests/icon-whith.svg"
+})
+st3.addEventListener("mouseleave", () =>{
+    stImg3.src = "../asests/icon-dark.svg"
+})
+st4.addEventListener("mouseenter", () =>{
+    stImg4.src = "../asests/icon2-whith.svg"
+})
+st4.addEventListener("mouseleave", () =>{
+    stImg4.src = "../asests/icon2-dark.svg"
+})
+st5.addEventListener("mouseenter", () =>{
+    stImg5.src = "../asests/icon3-whith.svg"
+})
+st5.addEventListener("mouseleave", () =>{
+    stImg5.src = "../asests/icon3-dark.svg"
+})
+st6.addEventListener("mouseenter", () =>{
+    stImg6.src = "../asests/icon4-whith.svg"
+})
+st6.addEventListener("mouseleave", () =>{
+    stImg6.src = "../asests/icon4-dark.svg"
+})
+st7.addEventListener("mouseenter", () =>{
+    stImg7.src = "../asests/icon5-whith.svg"
+})
+st7.addEventListener("mouseleave", () =>{
+    stImg7.src = "../asests/icon5-dark.svg"
+})
+
+st8.addEventListener("mouseenter", () =>{
+    stImg8.src = "../asests/icon6-whith.svg"
+})
+st8.addEventListener("mouseleave", () =>{
+    stImg8.src = "../asests/icon6-dark.svg"
+})
+st9.addEventListener("mouseenter", () =>{
+    stImg9.src = "../asests/icon7-whith.svg"
+})
+st9.addEventListener("mouseleave", () =>{
+    stImg9.src = "../asests/icon7-dark.svg"
+})
+st10.addEventListener("mouseenter", () =>{
+    stImg10.src = "../asests/icon8-whith.svg"
+})
+st10.addEventListener("mouseleave", () =>{
+    stImg10.src = "../asests/icon8-dark.svg"
+})
+st11.addEventListener("mouseenter", () =>{
+    stImg11.src = "../asests/icon9-whith.svg"
+})
+st11.addEventListener("mouseleave", () =>{
+    stImg11.src = "../asests/icon9-dark.svg"
+})
+st12.addEventListener("mouseenter", () =>{
+    stImg12.src = "../asests/icon10-whith.svg"
+})
+st12.addEventListener("mouseleave", () =>{
+    stImg12.src = "../asests/icon10-dark.svg"
+})
+st13.addEventListener("mouseenter", () =>{
+    stImg13.src = "../asests/icon11-whith.svg"
+})
+st13.addEventListener("mouseleave", () =>{
+    stImg13.src = "../asests/icon11-dark.svg"
+})
+st14.addEventListener("mouseenter", () =>{
+    stImg14.src = "../asests/icon12-whith.svg"
+})
+st14.addEventListener("mouseleave", () =>{
+    stImg14.src = "../asests/icon12-dark.svg"
+})
+st15.addEventListener("mouseenter", () =>{
+    stImg15.src = "../asests/icon13-whith.svg"
+})
+st15.addEventListener("mouseleave", () =>{
+    stImg15.src = "../asests/icon13-dark.svg"
+})
+st16.addEventListener("mouseenter", () =>{
+    stImg16.src = "../asests/icon14-whith.svg"
+})
+st16.addEventListener("mouseleave", () =>{
+    stImg16.src = "../asests/icon14-dark.svg"
+})
